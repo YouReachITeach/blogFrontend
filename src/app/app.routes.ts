c10c57cbe3942d06e5f6b6de2a492074f385a6d0
Levin <147081970+YouReachITeach@users.noreply.github.com>
2025-12-07 20:42:07 +0100

    Created Navbar, Home, archive, about and router entries

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {Home} from './home/home';
+import {About} from './about/about';
+import {Archive} from './archive/archive';
+export const routes: Routes = [
+  {path: "home", component: Home},
+  {path: "about", component: About},
+  {path: "archive", component: Archive},
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {path: "**", redirectTo: "home"}
+];
---
+++ src/app/archive/archive.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archive',
+  imports: [],
+  standalone: true,
+  templateUrl: './archive.html',
+  styleUrl: './archive.css',
+})
+export class Archive {
+
+}
